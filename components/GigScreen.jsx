eda40835dee7f9755dfaec34245f7201aea3faec
becomingjohndoe <karl.swatman@gmail.com>
2021-12-08 14:14:57 +0000

    added sign out button

--- components/GigScreen.jsx
+++ components/GigScreen.jsx
@@ -1,48 +1,53 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { useState } from "react";
 import { getGigsForHomepage } from "../utils/api";
 import { ScrollView } from "react-native-gesture-handler";
+import { signOutUser } from "../firebase";
 
 export default function GigScreen() {
-  const [results, setResults] = useState([]);
+	const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    getGigsForHomepage()
-      .then((results) => {
-        setResults(results);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+	useEffect(() => {
+		getGigsForHomepage()
+			.then((results) => {
+				setResults(results);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-
-  return (
-    <ScrollView>
-    <View style={styles.container}>
-      {results.map((gig)=>{
-        return ( 
-          <View key={gig.id}>
-            <Text>{gig.name}</Text>
-            <Text>{gig.dates.start.localDate}</Text>
-            <Text>{gig.dates.status.code}</Text>
-            <Text>{gig._embedded.venues[0].name}</Text>
-            <Image source={{uri:gig.images[4].url}} style={{ width: 375, height: 200 }}/>
-          </View>        )
-      })}
-      <StatusBar style="auto" />
-    </View>
-    </ScrollView>
-  );
+	return (
+		<ScrollView>
+			<Button title="sign out" onPress={signOutUser} />
+			<View style={styles.container}>
+				{results.map((gig) => {
+					return (
+						<View key={gig.id}>
+							<Text>{gig.name}</Text>
+							<Text>{gig.dates.start.localDate}</Text>
+							<Text>{gig.dates.status.code}</Text>
+							<Text>{gig._embedded.venues[0].name}</Text>
+							<Image
+								source={{ uri: gig.images[4].url }}
+								style={{ width: 375, height: 200 }}
+							/>
+						</View>
+					);
+				})}
+				<StatusBar style="auto" />
+			</View>
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
