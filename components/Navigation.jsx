925d505f84ef01e9580991f20491775ae81d8e87
becomingjohndoe <karl.swatman@gmail.com>
2021-12-10 10:43:33 +0000

    adds chats screen

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,25 +4,31 @@ import Profile from "./Profile";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import UserProfile from "./UserProfile";
 import { Button } from "react-native";
+import Chats from "./Chats";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
-    return (
-       <Drawer.Navigator initialRouteName="Home">
-           <Drawer.Screen name="Home" component={GigScreen} options={{
-									headerTitle: "Gigs",
-									headerRight: () => (
-										<Button
-											onPress={() => alert("This is a button!")}
-											title="Options"
-											color="gray"
-										/>
-									),
-								}}/>
-           <Drawer.Screen name="Profile" component={UserProfile} />
-       </Drawer.Navigator >
-    )
-}
+	return (
+		<Drawer.Navigator initialRouteName="Home">
+			<Drawer.Screen
+				name="Home"
+				component={GigScreen}
+				options={{
+					headerTitle: "Gigs",
+					headerRight: () => (
+						<Button
+							onPress={() => alert("This is a button!")}
+							title="Options"
+							color="gray"
+						/>
+					),
+				}}
+			/>
+			<Drawer.Screen name="Profile" component={UserProfile} />
+			<Drawer.Screen name="Chats" component={Chats} />
+		</Drawer.Navigator>
+	);
+};
 
 export default DrawerNavigation;
