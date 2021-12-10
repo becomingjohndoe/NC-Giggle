13869f6dc4f148b62c4d7ef03f33f20e6b9f4d06
Rafael <rafael.mendes1101@gmail.com>
2021-12-10 11:59:45 +0000

    Added filterGigs api request

--- utils/api.js
+++ utils/api.js
@@ -18,3 +18,21 @@ export const getGigsForHomepage = () => {
       console.dir(err);
     });
 };
+
+export const filterGigs = (genreId, sort) => {
+  return ticketmasterApi
+    .get(
+      `discovery/v2/events?apikey=AQ2psvkdE5dP1AD9SEmAPZ1Kb5AALDCG&locale=en-gb&size=50&segmentId=KZFzniwnSyZfZ7v7nJ&genreId=${genreId}&sort=${sort}&startDateTime=${currentDate.getFullYear()}-${
+        currentDate.getMonth() + 1
+      }-${currentDate.getDate()}T00:00:00Z`
+    )
+    .then((response) => {
+      return response.data._embedded.events.filter((events, i) => {
+        return (
+          events.dates.status.code !== "cancelled" &&
+          events.dates.status.code !== "offsale"
+        );
+      });
+    })
+    .catch(console.dir);
+};
