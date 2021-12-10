c557b57507510a452af49a3c903b9efad3f6195d
Rafael <rafael.mendes1101@gmail.com>
2021-12-10 12:00:43 +0000

    Added Appcontext

--- App.js
+++ App.js
@@ -9,64 +9,70 @@ import Home from "./components/Home";
 import { onAuthStateChanged, getAuth } from "@firebase/auth";
 import { checkNewuser } from "./firebase";
 import Profile from "./components/Profile";
-import GigScreen from './components/GigScreen';
+import GigScreen from "./components/GigScreen";
 import DrawerNavigation from "./components/Navigation";
-
+import { AppContext } from "./context/context";
 export default function App() {
-	const [user, setUser] = React.useState(null);
-	const [isLoading, setIsLoading] = React.useState(true);
-	const [isNewUser, setIsNewUser] = React.useState(true);
-	const auth = getAuth();
-	const Stack = createNativeStackNavigator();
+  const [user, setUser] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isNewUser, setIsNewUser] = React.useState(true);
+  const [currentGigs, setCurrentGigs] = React.useState([{}]);
+  const auth = getAuth();
+  const Stack = createNativeStackNavigator();
 
-	// check if user is loggen into firebase
-	useEffect(() => {
-		setIsLoading(true);
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				console.log("user is logged in");
-				setUser(user);
-				checkNewuser().then((u) => {
-					setIsNewUser(u);
-				});
-			} else {
-				console.log("user is not logged in");
-				setUser(null);
-			}
-			setIsLoading(false);
-		});
-	}, []);
-	if (isLoading) return <Text>Loading...</Text>;
-	return (
-		<>
-		<NavigationContainer>
-			<Stack.Navigator>
-				{user ? (
-					<>
-						{isNewUser ? (
-							<>
-								<Stack.Screen
-									name="Profile"
-									component={Profile}
-									options={{ headerShown: false }}
-									initialParams={{ newUser: setIsNewUser }}
-								/>
-							</>
-						) : (
-							<Stack.Group>
-							<Stack.Screen name="navigator" component={DrawerNavigation} options={{ headerShown: false }}/>
-							</Stack.Group>
-						)}
-					</>
-				) : (
-					<>
-						<Stack.Screen name="Login" component={Login} />
-						<Stack.Screen name="Sign Up" component={SignUp} />
-					</>
-				)}
-			</Stack.Navigator>
-		</NavigationContainer>
-</>
-);
-
+  // check if user is loggen into firebase
+  useEffect(() => {
+    setIsLoading(true);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("user is logged in");
+        setUser(user);
+        checkNewuser().then((u) => {
+          setIsNewUser(u);
+        });
+      } else {
+        console.log("user is not logged in");
+        setUser(null);
+      }
+      setIsLoading(false);
+    });
+  }, []);
+  if (isLoading) return <Text>Loading...</Text>;
+  return (
+    <>
+      <NavigationContainer>
+        <AppContext.Provider value={{ currentGigs, setCurrentGigs }}>
+          <Stack.Navigator>
+            {user ? (
+              <>
+                {isNewUser ? (
+                  <>
+                    <Stack.Screen
+                      name="Profile"
+                      component={Profile}
+                      options={{ headerShown: false }}
+                      initialParams={{ newUser: setIsNewUser }}
+                    />
+                  </>
+                ) : (
+                  <Stack.Group>
+                    <Stack.Screen
+                      name="navigator"
+                      component={DrawerNavigation}
+                      options={{ headerShown: false }}
+                    />
+                  </Stack.Group>
+                )}
+              </>
+            ) : (
+              <>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Sign Up" component={SignUp} />
+              </>
+            )}
+          </Stack.Navigator>
+        </AppContext.Provider>
+      </NavigationContainer>
+    </>
+  );
 }
