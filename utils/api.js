8531dfeb3b902f236b426e031ba10c773b32352a
Rafael <rafael.mendes1101@gmail.com>
2021-12-10 16:16:22 +0000

    Added filters to api request function

--- utils/api.js
+++ utils/api.js
@@ -4,22 +4,8 @@ const ticketmasterApi = axios.create({
   baseURL: "https://app.ticketmaster.com/",
 });
 
-export const getGigsForHomepage = () => {
-  return ticketmasterApi
-    .get(
-      "discovery/v2/events?apikey=AQ2psvkdE5dP1AD9SEmAPZ1Kb5AALDCG&locale=en-gb&size=10&segmentId=KZFzniwnSyZfZ7v7nJ"
-    )
-    .then((response) => {
-      // setResults(response);
-      // console.log(response.data._embedded.events);
-      return response.data._embedded.events;
-    })
-    .catch((err) => {
-      console.dir(err);
-    });
-};
-
-export const filterGigs = (genreId, sort) => {
+export const getGigsForHomePage = (genreId, sort) => {
+  const currentDate = new Date();
   return ticketmasterApi
     .get(
       `discovery/v2/events?apikey=AQ2psvkdE5dP1AD9SEmAPZ1Kb5AALDCG&locale=en-gb&size=50&segmentId=KZFzniwnSyZfZ7v7nJ&genreId=${genreId}&sort=${sort}&startDateTime=${currentDate.getFullYear()}-${
