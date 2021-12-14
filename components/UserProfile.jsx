cd58d7fbbe4f1c28a98140df5556690c41029ad3
becomingjohndoe <karl.swatman@gmail.com>
2021-12-14 22:55:47 +0000

    fixes loading state and initial render og home screen

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -2,25 +2,26 @@ import React, { useContext } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { signOutUser } from "../firebase";
 import { UserContext } from "../context/context";
-const UserProfile = () => {
-  const { setUserParams } = useContext(UserContext);
-  return (
-    <>
-      <Text>Hello</Text>
+const UserProfile = ({ navigation, route }) => {
+	const { setUserParams } = useContext(UserContext);
+	return (
+		<>
+			<Text>Hello</Text>
 
-      <Button
-        title="sign out"
-        onPress={() => {
-          signOutUser().then(() => {
-            setUserParams({
-              city: "",
-              genre: "",
-            });
-          });
-        }}
-      />
-    </>
-  );
+			<Button
+				title="sign out"
+				onPress={() => {
+					signOutUser().then(() => {
+						setUserParams({
+							city: "",
+							genre: "",
+						});
+						route.params.newUser(false);
+					});
+				}}
+			/>
+		</>
+	);
 };
 
 export default UserProfile;
